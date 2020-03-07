256172fc8f5f4140402a32be7e25002e5a7920a9
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-03-07 13:08:26 -0600

    yay - I finished ex3, but I feel like I am missing something

--- ex3/script.js
+++ ex3/script.js
@@ -1,7 +1,8 @@
 // Make the box blue when you click the button. (Here's some code to help you get started)
 
-var button = document.getElementById("myButton");
+let button = document.getElementById("myButton");
 
 button.addEventListener('click', function(){
+    box.style.backgroundColor='blue'
     
 })
