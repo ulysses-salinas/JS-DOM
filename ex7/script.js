a20c02c926d5bbd1fe148ea29b1453f34c11cfec
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-03-10 18:50:22 -0500

    yay - I finished ex7

--- ex7/script.js
+++ ex7/script.js
@@ -1,3 +1,19 @@
 // Make the section disappear and reappear whenever you click the section-header
 // BONUS: Can you make the section slide up and down like this? :
 // https://www.w3schools.com/bootstrap/bootstrap_collapse.asp
+
+let accordion = document.getElementById("accordion")
+let sectionHeader = document.getElementById("section-header")
+let hideSection = document.getElementById("section")
+
+
+sectionHeader.addEventListener('click', function(){
+
+       if (hideSection.style.display === "none") {
+           hideSection.style.display = "block";
+        } else {
+             hideSection.style.display = "none";
+            }
+}
+)
+
